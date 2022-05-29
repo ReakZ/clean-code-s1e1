d7b56f163bf403ef373a56c3cb223b90c707d51a
ReakZ <spec.kov@list.ru>
2022-05-29 15:22:46 +0300

    fix: replace alt description for decorative images with empty according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ var createNewTaskElement=function(taskString){
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     deleteButtonImg.src='./remove.svg';
-    deleteButtonImg.alt='del-btn';
+    deleteButtonImg.alt='';
     deleteButton.appendChild(deleteButtonImg);
 
 
